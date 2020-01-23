e2cfde108e7502b80b9dec4441f22b45a674b550
jpantao <jp.antao@campus.fct.unl.pt>
2020-01-23 00:35:19 +0000

    added key verification accepting "GCS-TEST-KEY-00"

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+    if (argc == 2){
+        int k = 1005;
+        int sum = 0;
+
+        for(int i = 0; i < strlen(argv[1]); i++)
+            sum+= (int)argv[1][i];
+
+        if( sum == k )
+            printf("Access granted!");
+        else
+            printf("Acess denied!");
+
+    } else {
+        printf("Usage: %s <key>\n", argv[0]);
+    }
+
     return 0;
 }
