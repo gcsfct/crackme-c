fb6c1ac82872237be31a7fdac9ec5dca536ae34d
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-01-23 18:51:34 +0000

    Reformat code

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char* argv[]) {
-    if (argc == 2){
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
         int k = 1005;
         int sum = 0;
 
-        for(int i = 0; i < strlen(argv[1]); i++)
-            sum+= (int)argv[1][i];
+        for (int i = 0; i < strlen(argv[1]); i++)
+            sum += (int) argv[1][i];
 
-        if( sum == k )
+        if (sum == k)
             printf("Access granted!");
         else
             printf("Acess denied!");
